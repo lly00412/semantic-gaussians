5bb5c9165757060678e7ff83ac18d427e7a13f08
lly00412 <315885469@qq.com>
2025-03-26 16:12:42 -0400

    change color grad shape

--- sparse-channel-rasterization/rasterize_points.cu
+++ sparse-channel-rasterization/rasterize_points.cu
@@ -159,7 +159,7 @@ RasterizeGaussiansBackwardCUDA(
 
 	torch::Tensor dL_dmeans3D = torch::zeros({P, 3}, means3D.options());
 	torch::Tensor dL_dmeans2D = torch::zeros({P, 3}, means3D.options());
-	torch::Tensor dL_dcolors = torch::zeros({P, NUM_CHANNELS}, means3D.options());
+	torch::Tensor dL_dcolors = torch::zeros({P, TOP_K_LOGITS_CHANNELS}, means3D.options());
 	torch::Tensor dL_dconic = torch::zeros({P, 2, 2}, means3D.options());
 	torch::Tensor dL_dopacity = torch::zeros({P, 1}, means3D.options());
 	torch::Tensor dL_dcov3D = torch::zeros({P, 6}, means3D.options());
